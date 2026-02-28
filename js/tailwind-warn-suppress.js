37875acf844a02b209bae5589ccc92de4331d969
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2026-03-01 00:22:03 +0800

    fix

--- js/tailwind-warn-suppress.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const originalWarn = console.warn;
-    console.warn = function (...args) {
-        if (args[0] && typeof args[0] === 'string' && args[0].includes('cdn.tailwindcss.com should not be used in production')) {
-            return;
-        }
-        originalWarn.apply(console, args);
-    };
-})();
